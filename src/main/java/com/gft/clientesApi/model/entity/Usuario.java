ac7c438704304477a38f4175513cea5e934b0ae1
roiu <roberto.ribeiro@gft.com>
2022-02-17 09:55:33 -0300

    created entity usuario

---
+++ src/main/java/com/gft/clientesApi/model/entity/Usuario.java
@@ -0,0 +1,27 @@
+package com.gft.clientesApi.model.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, name = "login")
+    private String username;
+
+    @Column(name = "senha")
+    private String pasword;
+}
