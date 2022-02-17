34239b59ab37523198d7f4d7b3df633c17d07eb6
roiu <roberto.ribeiro@gft.com>
2022-02-17 10:16:37 -0300

    created controller for entity usuario

---
+++ src/main/java/com/gft/clientesApi/model/controller/UsuarioController.java
@@ -0,0 +1,25 @@
+package com.gft.clientesApi.model.controller;
+
+import com.gft.clientesApi.model.entity.Usuario;
+import com.gft.clientesApi.model.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void salvar(@RequestBody Usuario usuario) {
+        usuarioRepository.save(usuario);
+    }
+}
